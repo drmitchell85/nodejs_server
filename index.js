b411b5bbc6b9bdadd9324031dde8e6ed87da49ac
Dan Mitchell <daniel_mitchell@comast.com>
2020-02-28 11:59:37 -0500

    commit1

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// https://git.heroku.com/obscure-spire-73695.git 
+
 // import express library
 // require gets access to the express library
 const express = require('express');
@@ -10,4 +12,6 @@ app.get('/', (req, res) => {
     res.send({ hi: 'there'});
 });
 
-app.listen(5000);  
+// Heroku will inject the port to use while in production OR use local port while in dev env
+const PORT = process.env.PORT || 5000;
+app.listen(PORT); 
